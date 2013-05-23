4f789158969ac95e18b470732d5a20a8b5d2bc90
Carlo Smouter <carlo.smouter@moccu.com>
2013-05-23 18:13:45 +0200

    interface.

---
+++ wowapi/api.py
@@ -0,0 +1,23 @@
+import logging
+import requests
+
+
+logger = logging.getLogger('wowapi')
+
+
+class WoWAPI(object):
+
+    def __init__(self):
+        pass
+
+    def get_item(self):
+        pass
+        # return ItemObject
+
+    def get_auctions(self):
+        pass
+        # return AuctionObject
+
+    def get_character(self):
+        pass
+        # return CharacterObject
