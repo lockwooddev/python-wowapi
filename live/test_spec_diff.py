3081a2f8a55123221ef76b9e5bf4bdfad8936f19
Lockwood <lockwooddev@gmail.com>
2019-08-16 14:00:01 +0300

    Add spec downloader and spec diff checker

---
+++ live/test_spec_diff.py
@@ -0,0 +1,31 @@
+import subprocess
+import tempfile
+
+import requests
+
+
+def check_spec(id, spec_file):
+    print('Detecting changes in the {0}'.format(id))
+    base_url = 'https://develop.battle.net/assets/api/'
+    spec_url = '{0}{1}'.format(base_url, spec_file)
+
+    response = requests.get(spec_url)
+    new_spec_file = tempfile.NamedTemporaryFile()
+    new_spec_file.write(response.content)
+    new_spec_file.seek(0)
+
+    result = subprocess.run(["diff", spec_file, new_spec_file.name], capture_output=True)
+    if bool(result.stdout):
+        print('Changes in the {0} have been detected since last release!'.format(id))
+        print('I need a human to look for possible deprecations..')
+        print('-----------------------------------------------------')
+
+        lines = result.stdout.decode('utf-8').split('\n')
+        [print(l) for l in lines]
+    else:
+        print('No API changes detected..')
+
+
+if __name__ == '__main__':
+    check_spec('WoW Community API', 'wow-community-api.json')
+    check_spec('WoW Game Data API', 'wow-game-data-api.json')
