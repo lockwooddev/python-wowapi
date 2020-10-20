7b3e662aae0fa66aefebe8613a58263e5c2f9404
Trevor Phillips <trevorcoreyphillips@gmail.com>
2020-10-20 02:27:10 -0600

    Updated game_data.py example (#21)

--- examples/game_data.py
+++ examples/game_data.py
@@ -17,17 +17,21 @@ logger.info('Game Data API Example')
 
 # fetch token price for region
 api = WowApi(os.environ['WOW_CLIENT_ID'], os.environ['WOW_CLIENT_SECRET'])
-data = api.get_token('eu', namespace='dynamic-eu', locale='de_DE')
+data = api.get_token_index('eu', namespace='dynamic-eu', locale='de_DE')
 pprint(data)
 
 # get realm list and request detail href with the get_data_resource method
-data = api.get_realms('us', namespace='dynamic-us')
+data = api.get_realm_index('eu', namespace='dynamic-eu', locale='de_DE')
 detail_url = data['realms'][0]['key']['href']
-detail_data = api.get_data_resource(detail_url, region='us')
+detail_data = api.get_data_resource(detail_url, region='eu', locale='de_DE')
 pprint(detail_data)
 
 # get playable specializations list and fetch a single specialization with the api
-data = api.get_playable_specializations('us', namespace='static-us')
+data = api.get_playable_specialization_index(
+    'eu', namespace='static-eu', locale='de_DE'
+)
 spec_id = data['character_specializations'][0]['id']
-specialization = api.get_playable_specialization('us', namespace='static-us', spec_id=spec_id)
+specialization = api.get_playable_specialization(
+    'eu', namespace='static-eu', spec_id=spec_id, locale='de_DE'
+)
 pprint(specialization)
