db1da71d43d9b09744254bfca86fe14920ea7ce0
Trevor Phillips <trevorcoreyphillips@gmail.com>
2020-10-19 21:34:35 -0600

    Update game_data.py
    flake8

--- examples/game_data.py
+++ examples/game_data.py
@@ -5,29 +5,33 @@ from pprint import pprint
 from wowapi import WowApi
 
 
-logger = logging.getLogger('wowapi')
+logger = logging.getLogger("wowapi")
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-logger.info('Game Data API Example')
+logger.info("Game Data API Example")
 
 # fetch token price for region
-api = WowApi(os.environ['WOW_CLIENT_ID'], os.environ['WOW_CLIENT_SECRET'])
-data = api.get_token_index('eu', namespace='dynamic-eu', locale='de_DE')
+api = WowApi(os.environ["WOW_CLIENT_ID"], os.environ["WOW_CLIENT_SECRET"])
+data = api.get_token_index("eu", namespace="dynamic-eu", locale="de_DE")
 pprint(data)
 
 # get realm list and request detail href with the get_data_resource method
-data = api.get_realm_index('eu', namespace='dynamic-eu', locale='de_DE')
-detail_url = data['realms'][0]['key']['href']
-detail_data = api.get_data_resource(detail_url, region='eu', locale='de_DE')
+data = api.get_realm_index("eu", namespace="dynamic-eu", locale="de_DE")
+detail_url = data["realms"][0]["key"]["href"]
+detail_data = api.get_data_resource(detail_url, region="eu", locale="de_DE")
 pprint(detail_data)
 
 # get playable specializations list and fetch a single specialization with the api
-data = api.get_playable_specialization_index('eu', namespace='static-eu', locale='de_DE')
-spec_id = data['character_specializations'][0]['id']
-specialization = api.get_playable_specialization('eu', namespace='static-eu', spec_id=spec_id, locale='de_DE')
+data = api.get_playable_specialization_index(
+    "eu", namespace="static-eu", locale="de_DE"
+)
+spec_id = data["character_specializations"][0]["id"]
+specialization = api.get_playable_specialization(
+    "eu", namespace="static-eu", spec_id=spec_id, locale="de_DE"
+)
 pprint(specialization)
